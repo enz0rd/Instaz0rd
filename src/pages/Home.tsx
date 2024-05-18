f8f9852f301051c75b3f5578ea77755194232e6d
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2024-05-18 07:05:08 -0300

    small changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,9 @@
-export default function Login() {
+export default function Home() {
+    if(!document.cookie.token || document.cookie.token === 'undefined' || document.cookie.token === null) {
+        window.location.href = '/signin';
+        return;
+    }
+    
     return (
         <div>testasdasdaaaaasde</div>
     )
