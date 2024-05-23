d09b6c409595a8eb304b2dfbe5f528a4282da05a
Enzo <enzorossidaltoe@hotmail.com.br>
2024-05-23 17:57:08 -0300

    stories

--- src/pages/Stories.tsx
+++ src/pages/Stories.tsx
@@ -1,20 +1,38 @@
 import Header from "@/components/Header";
-
+import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
 
 export default function Stories() {
 
     return (
         <>
         { window.innerWidth > 768 ? (
+            <>
                 <Header />
+                <div className="h-screen flex items-center justify-center ml-2 mr-2 mt-10 ">
+                    <div className="absolute top-[6rem] flex gap-3 items-center scale-[50%]">
+                        <div className="w-[4rem] h-[4rem] rounded-full bg-zinc-50" />
+                        <h1 className="text-4xl text-zinc-50 text-center">User</h1>
+                    </div>
+                    <img src="/api/src/assets/under-construction.png" alt="Under construction" className="h-[90%] border-zinc-50 border-2 aspect-story rounded-xl bg-zinc-800 object-cover" />
+                </div>
+            </>
+            ) : (
+                <>
+                    <div className="h-screen flex items-center justify-center ml-2 mr-2 ">
+                        <div className="absolute top-[1rem] flex gap-3 items-center scale-[50%]">
+                            <div className="w-[4rem] h-[4rem] rounded-full bg-zinc-50" />
+                            <h1 className="text-4xl text-zinc-50 text-center">User</h1>
+                        </div>
+                        <div>
+                        </div>
+                        <img src="/api/src/assets/under-construction.png" alt="Under construction" className="h-screen aspect-story bg-zinc-800 object-cover" />
+                    </div>
+                </>
             )
         }
-            <div className="h-screen flex items-center justify-center mt-10">
-                <div className="absolute top-[6rem] flex gap-3 items-center scale-[50%]">
-                    <div className="w-[4rem] h-[4rem] rounded-full bg-zinc-50" />
-                    <h1 className="text-4xl text-zinc-50 text-center">User</h1>
-                </div>
-                <img src="/api/src/assets/under-construction.png" alt="Under construction" className="h-[90%] border-zinc-50 border-2 aspect-story w-[90%] rounded-xl bg-zinc-800 object-cover" />
+            <div className="absolute top-[50%] flex flex-row">
+                <FaCircleChevronLeft className="absolute left-[2rem]"/>
+                <FaCircleChevronRight className="absolute right-[6rem]"/>
             </div>
         </>
     )
